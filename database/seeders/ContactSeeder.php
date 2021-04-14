28c9d46fc436b8b71d84352524d401cc05823230
windel <wendz.v14@gmail.com>
2021-04-14 08:31:50 +0800

    Added Contact model, migration and seeder

---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Contact::create([
+            'name' => 'value',
+            'contact_number' => 'value'
+        ]);
+        //
+    }
+}
