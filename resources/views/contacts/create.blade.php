f5e2c6e20126ccb58e66a622b3a0ab5eb3517f46
windel <wendz.v14@gmail.com>
2021-04-22 16:43:39 +0800

    laravel CRUD error Message

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -6,38 +6,39 @@
         <div class="pull-left">
             <h2>Add New contacts</h2>
         </div>
+        
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('contacts.index') }}"> Back</a>
         </div>
     </div>
 
-    @if ($errors->any())
-    <div class="alert alert-danger">
+   
   
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
 
 <form action="{{ route('contacts.store') }}" method="POST">
-    @csrf
+    @csrf 
   
-     <div class="row">
+    
+    <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Enter name">
+            <div class="form-group"> 
+                <strong>Full Name:</strong>
+                <input type="text" name="name" class="form-control" id="name" required aria-required="true" placeholder="Enter name" >
+                
             </div>
         </div>
+       
+      <div class="field">
         <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
+            <div class="form-group {{ $errors->has('contact_number') ? 'has-error' : ''}}">
                 <strong>Contact Number:</strong>
-                <input type="number" name="contact_number" class="form-control" placeholder="Enter Contact Number">
-            </div>
+                <input class="form-control" required="required" name="contact_number" type="number" id="contact_number" placeholder="Enter Contact Number">
+                 {!! $errors->first('contact_number', '<p class="help-block">:message</p>') !!}
+          </div>
         </div>
+     </div>
+            
+        
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                 <button type="submit" class="btn btn-primary">Submit</button>
         </div>
--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -16,20 +16,25 @@
     <form action="{{ route('contacts.update',$row->id) }}" method="POST">
         @csrf
         @method('PUT')
-   
-         <div class="row">
+     
+        <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Full Name:</strong>
-                    <input type="text" name="name" value="{{ $row->name }}" class="form-control" placeholder="Full Name">
+                    <input type="text" name="name" value="{{ $row->name }}" class="form-control" id="name" required aria-required="true" placeholder="Full Name">
                 </div>
             </div>
+            
+         
             <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+                <div class="form-group {{ $errors->has('contact_number') ? 'has-error' : ''}}">
                     <strong>Contact Number:</strong>
-                    <input type="number" name="contact_number" value="{{ $row->contact_number }}" class="form-control" placeholder="Contact Number">
+                    <input class="form-control" required="required" name="contact_number" value="{{$row->contact_number  }}" type="number" id="contact_number">
+                 {!! $errors->first('contact_number', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
+        
+
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
               <button type="submit" class="btn btn-primary">Submit</button>
             </div>
